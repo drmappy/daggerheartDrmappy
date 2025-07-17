0771753a98220e004f26413f2a1e1573fb2fbfb7
Massimiliano <massimarchel@gmail.com>
2025-07-17 16:35:39 -0400

    Realization that I need to make a account parent that splits into player and creator. This will be usefull for example: a player who wants to see all of they're characters.

--- playerClient/src/main/java/dnd/perso/playerclient/controller/PlayerController.java
+++ playerClient/src/main/java/dnd/perso/playerclient/controller/PlayerController.java
@@ -1,11 +1,104 @@
 package dnd.perso.playerclient.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import dnd.perso.playerclient.service.Player;
+import dnd.perso.playerclient.service.dto.*;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/player")
 @CrossOrigin(origins = "http://localhost:5173")
 public class PlayerController {
+    private final Player playerService;
+    public PlayerController(Player playerService) {
+        this.playerService = playerService;
+    }
+    @GetMapping("/{name}")
+    public ResponseEntity<DaggerheartClassDTO> getDaggerHeartClass(@PathVariable String name) {
+        try{
+            return ResponseEntity.ok().body(playerService.getClassById(name));
+        }catch (Exception e){
+            return ResponseEntity.notFound().build();
+        }
+    }
+    @GetMapping("/{name}")
+    public ResponseEntity<SubClassDTO> getSubClass(@PathVariable String name) {
+        try{
+            return ResponseEntity.ok().body(playerService.getSubClassById(name));
+        }catch (Exception e){
+            return ResponseEntity.notFound().build();
+        }
+    }
+    @PostMapping("/characterCreation")
+    public ResponseEntity<Void> createCharacter(@RequestBody DaggerheartCharacterDTO daggerheartCharacterDTO) {
+        try {
+            playerService.saveCharacter(daggerheartCharacterDTO);
+            return ResponseEntity.ok().build();
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+    @GetMapping("characterVue")
+    public ResponseEntity<DaggerheartCharacterDTO> getCharacterVue(@RequestBody Long id) {
+        try {
+            DaggerheartCharacterDTO character = playerService.getCharacterById(id);
+            return ResponseEntity.ok(character);
+        } catch (Exception e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+    @GetMapping("/ancestry")
+    public ResponseEntity<AncestryDTO>getAncestry(@RequestBody String name) {
+        try {
+            AncestryDTO ancestry = playerService.getAncestryById(name);
+            return ResponseEntity.ok(ancestry);
+        } catch (Exception e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+    @GetMapping("/community")
+    public ResponseEntity<CommunityDTO> getCommunity(@RequestBody String name){
+        try {
+            CommunityDTO community = playerService.getCommunityById(name);
+            return ResponseEntity.ok(community);
+        } catch (Exception e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+    @GetMapping("/feature")
+    public ResponseEntity<FeatureDTO> getFeature(@RequestBody String name){
+        try {
+            FeatureDTO feature = playerService.getFeatureById(name);
+            return ResponseEntity.ok(feature);
+        } catch (Exception e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+    @GetMapping("/weapon")
+    public ResponseEntity<WeaponDTO> getWeapon(@RequestBody String name) {
+        try {
+            WeaponDTO weapon = playerService.getWeaponById(name);
+            return ResponseEntity.ok(weapon);
+        } catch (Exception e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+    @GetMapping("armor")
+    public ResponseEntity<ArmorDTO> getArmor(@RequestBody String name) {
+        try {
+            ArmorDTO armor = playerService.getArmorById(name);
+            return ResponseEntity.ok(armor);
+        } catch (Exception e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+    @GetMapping("experience")
+    public ResponseEntity<ExperienceDTO> getExperience(@RequestBody Long id) {
+        try {
+            ExperienceDTO experience = playerService.getExperienceById(id);
+            return ResponseEntity.ok(experience);
+        } catch (Exception e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
